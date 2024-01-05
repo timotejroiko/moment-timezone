e89407ea6e3e58179745d6f27ec62ac1936b5fef
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2024-01-05 16:36:11 -0300

    test

--- scripts/check.js
+++ scripts/check.js
@@ -3,9 +3,7 @@ fetch("https://api.github.com/repos/JodaOrg/global-tz/releases/latest").then(x =
     const remote = json.tag_name.replace("gtz", "");
     if(current !== remote) {
         console.log("Update available");
-        process.exit(0);
     } else {
         console.log("Already up to date");
-        process.exit(1);
     }
 });
