bbb54b6ceae5a8336d1304ae10c5f45766715158
timotejroiko <33236065+timotejroiko@users.noreply.github.com>
2026-03-08 02:06:27 +0000

    Apply automatic changes

--- tests/zones/antarctica/casey.js
+++ tests/zones/antarctica/casey.js
@@ -4,9 +4,9 @@ var helpers = require("../../helpers/helpers");
 
 exports["Antarctica/Casey"] = {
 
-	"guess:by:offset" : helpers.makeTestGuess("Antarctica/Casey", { offset: true }),
+	"guess:by:offset" : helpers.makeTestGuess("Antarctica/Casey", { offset: true, expect: "Asia/Manila" }),
 
-	"guess:by:abbr" : helpers.makeTestGuess("Antarctica/Casey", { abbr: true }),
+	"guess:by:abbr" : helpers.makeTestGuess("Antarctica/Casey", { abbr: true, expect: "Asia/Manila" }),
 
 	"1968" : helpers.makeTestYear("Antarctica/Casey", [
 		["1968-12-31T23:59:59+00:00", "23:59:59", "-00", 0]
--- tests/zones/antarctica/vostok.js
+++ tests/zones/antarctica/vostok.js
@@ -4,9 +4,9 @@ var helpers = require("../../helpers/helpers");
 
 exports["Antarctica/Vostok"] = {
 
-	"guess:by:offset" : helpers.makeTestGuess("Antarctica/Vostok", { offset: true }),
+	"guess:by:offset" : helpers.makeTestGuess("Antarctica/Vostok", { offset: true, expect: "Asia/Karachi" }),
 
-	"guess:by:abbr" : helpers.makeTestGuess("Antarctica/Vostok", { abbr: true }),
+	"guess:by:abbr" : helpers.makeTestGuess("Antarctica/Vostok", { abbr: true, expect: "Asia/Karachi" }),
 
 	"1957" : helpers.makeTestYear("Antarctica/Vostok", [
 		["1957-12-15T23:59:59+00:00", "23:59:59", "-00", 0],
